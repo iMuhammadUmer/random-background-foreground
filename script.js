ca1013bac53b8b031893a4c0f94ad3a4d20a640e
Muhammad Umer <mumer1297@gmail.com>
2020-08-07 09:37:06 +0500

    [feat] generating random color from array

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-var myArray = ["Apples", "Bananas", "Pears"];
+var colors = [
+  "#FF5733",
+  "#DAF7A6",
+  "#FFC300",
+  "#FF5733",
+  "#C70039",
+  "#900C3F",
+  "#F9FF33",
+  "#3933FF",
+  "#FF33E9",
+  "#FF3333",
+  "#8D5454",
+  "#54588D",
+  "#548D78",
+  "#9A5A5A",
+  "#FA0000",
+  "#B2B2B2",
+  "#FF5757",
+  "#5C991B",
+  "#101170",
+  "#39D28F",
+];
 
-var randomItem = myArray[Math.floor(Math.random() * myArray.length)];
-
-document.body.innerHTML = randomItem;
+var randCol1 = colors[Math.floor(Math.random() * colors.length)];
+var randCol2 = colors[Math.floor(Math.random() * colors.length)];
+console.log(randCol1);
+console.log(randCol2);
